a8b1d0740d2b996f0b765df37b55eb6b4d57d6c2
Victoria Yaakov <victoriaja1916@gmail.com>
2024-03-25 12:29:14 +0200

    removed helm

--- main.tf
+++ main.tf
@@ -4,21 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # helm = {
-    #   version = "~> 2.6.0"
-    # }
   }
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.eks_host
-#     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-#     token                  = module.eks.eks_token
-#   }
-# }
